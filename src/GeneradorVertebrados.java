8a3ec364cb9f0e8a311e1e9792bfb396c28d4245
ownavarro1 <ownavarro1@gmail.com>
2022-09-08 08:36:20 -0500

    Prueba Modificacion Vertebrado

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,9 +1,9 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Perro", "Pezcados5"); 
+        miVertebrados[0] = new Vertebrado("Camello2", "Mamifero2"); 
+        miVertebrados[1] = new Vertebrado("Vaca2", "Mamifero2");
+        miVertebrados[2] = new Vertebrado("Perro2", "Pezcados8"); 
         
         return miVertebrados;
     }
